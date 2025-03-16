05e668789d68c5eb27c8da28edcbaf8e401318e7
midbahou <midbahou@gmail.com>
2025-03-16 13:31:58 -0400

    install body-parser and morgan

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const app = express();
-const path = require('path');
-const PORT = 3000;
+const app = express(); // Create an Express application
+const path = require('path'); // Node.js module for handling file paths
+const PORT = 3000; // Define the port number
+
+
+const bodyParser = require('body-parser');// Body-parser: Middleware to parse incoming data
+const morgan = require('morgan'); //Morgan: HTTP request logger middleware
 
 // Middleware 
-app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
+app.use(bodyParser.json());// Parse JSON data from requests
+app.use(bodyParser.urlencoded({ extended: true })); // parse form data (URL-encoded)
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files (e.g. CSS)
+
 
 // Template Engine 
 app.set('view engine', 'ejs');
